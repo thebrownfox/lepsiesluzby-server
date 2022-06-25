31544de4e7561d5c94f3cf559ebba45fd759be5c
Marek "foxie" Brenčič <marek@brencic.sk>
2022-06-25 14:33:34 +0200

    fix: :alien: Fix jira api call

--- api/controllers/suggestions/suggestions.controller.js
+++ api/controllers/suggestions/suggestions.controller.js
@@ -8,29 +8,37 @@ const apiUrl = protocol + baseUrl + apiPath;
 // const baseUrl = "https://lepsiesluzby.atlassian.net/jira/rest/servicedeskapi/request";
 
 const jiraUser = process.env.JIRA_USER || "marek@brencic.sk";
-const jiraToken = process.env.JIRA_TOKEN || "LKsFWJnYqTJpullhYolV80D3"
+const jiraToken = process.env.JIRA_TOKEN;
 
 const token = Buffer.from(`${jiraUser}:${jiraToken}`).toString('base64')
 const headers = {
     "Authorization": `Basic ${token}`
 };
 
+const fieldPrefix = "customfield_";
+
+const fields = {
+    category: fieldPrefix + 10038
+}
+
 export default {
     getSuggestions: async (request, h) => {
         try {
-            const params = `jql=project = SDM  AND component = e-services AND status != "Nový podnet" ORDER BY created DESC&fields=summary,created&maxResults=5`;
-            const response = await Wreck.get(`${apiUrl}/search?${params}`);
-            return h.response(response);
+            const params = `jql=project = SDM  AND component = e-services AND status != "Nový podnet" ORDER BY created DESC&fields=summary,created,${fields.category}&maxResults=5`;
+            const { payload } = await Wreck.get(`${apiUrl}/search?${params}`);
+            return h.response(payload);
         } catch (error) {
             return Boom.boomify(error);
         }
     },
     createSuggestions: async (request, h) => {
         try {
-            const response = await Wreck.get(baseUrl, {
-                headers
+            const newSuggestion = request.payload;
+            const { payload } = await Wreck.post(baseUrl, {
+                headers,
+                payload: newSuggestion
             });
-            return h.response(response);
+            return h.response(payload);
         } catch (error) {
             return Boom.boomify(error);
         }
